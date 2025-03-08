0d3d353c547cad5732952d0bf19770b099c13c06
Alireza Mousavi <mousaviali1381@gmail.com>
2025-03-08 21:46:18 +0330

    go: chessboard

---
+++ go/chessboard/chessboard.go
@@ -0,0 +1,50 @@
+package chessboard
+
+type File []bool
+
+type Chessboard map[string]File
+
+// CountInFile returns how many squares are occupied in the chessboard,
+// within the given file.
+func CountInFile(cb Chessboard, file string) int {
+	count := 0
+	for _, col := range cb[file] {
+		if col {
+			count++
+		}
+	}
+
+	return count
+}
+
+// CountInRank returns how many squares are occupied in the chessboard,
+// within the given rank.
+func CountInRank(cb Chessboard, rank int) int {
+	if rank > 8 || rank < 1 {
+		return 0
+	}
+
+	count := 0
+	for _, row := range cb {
+		if row[rank-1] {
+			count++
+		}
+	}
+
+	return count
+}
+
+// CountAll should count how many squares are present in the chessboard.
+func CountAll(cb Chessboard) int {
+	return 64
+}
+
+// CountOccupied returns how many squares are occupied in the chessboard.
+func CountOccupied(cb Chessboard) int {
+	count := 0
+	for i := 1; i < 9; i++ {
+		count += CountInRank(cb, i)
+	}
+
+	return count
+}
