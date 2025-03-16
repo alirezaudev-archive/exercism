01125be6235591be4be95e2ec422db3fdd497a93
Alireza Mousavi <mousaviali1381@gmail.com>
2025-03-16 12:17:35 +0330

    go: strain

---
+++ go/strain/strain.go
@@ -0,0 +1,25 @@
+package strain
+
+func Keep[T any](collection []T, predicate func(T) bool) []T {
+	var result []T
+
+	for _, v := range collection {
+		if predicate(v) {
+			result = append(result, v)
+		}
+	}
+
+	return result
+}
+
+func Discard[T any](collection []T, predicate func(T) bool) []T {
+	var result []T
+
+	for _, v := range collection {
+		if !predicate(v) {
+			result = append(result, v)
+		}
+	}
+
+	return result
+}
